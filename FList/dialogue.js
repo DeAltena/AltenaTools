5d29f339bd6d078afb4616fcdfc58286c8b2baf1
Jan Hoffmann <hoffmann@student.tugraz.at>
2021-10-25 01:05:03 +0200

    fixed Whole-text functionality

--- FList/dialogue.js
+++ FList/dialogue.js
@@ -1,85 +1,101 @@
 initCollapsibles();
 
+let col, bold, italic, underline, strikethrough, coloured, rainbow, sub, sup;
+
+function startTags(){
+    let res = "";
+    if(coloured && !rainbow)
+        res += `[color=${col}]`;
+    if(bold)
+        res += `[b]`;
+    if(italic)
+        res += `[i]`;
+    if(underline)
+        res += `[u]`;
+    if(strikethrough)
+        res += `[s]`;
+    if(sub)
+        res += `[sub]`;
+    if(sup)
+        res += `[sup]`;
+
+    return res;
+}
+
+function endTags(){
+    let res = "";
+    if(sup)
+        res += `[/sup]`;
+    if(sub)
+        res += `[/sub]`;
+    if(strikethrough)
+        res += `[/s]`;
+    if(underline)
+        res += `[/u]`;
+    if(italic)
+        res += `[/i]`;
+    if(bold)
+        res += `[/b]`;
+    if(coloured && !rainbow)
+        res += `[/color]`;
+
+    return res;
+}
+
 function colorizeDialogue(){
     let str = document.getElementById("textarea").value,
-        col = document.getElementById("colour").value,
         start = 0, newBegin = 0, out = "",
-        bold = document.getElementById("bold").checked,
-        italic = document.getElementById("italic").checked,
-        underline = document.getElementById("underline").checked,
-        strikethrough = document.getElementById("strikethrough").checked,
-        coloured = !isBlank(col),
-        rainbow = document.getElementById("rainbow").checked,
-        sub = document.getElementById("sub").checked,
-        sup = document.getElementById("super").checked,
         startTag = document.getElementById("starttag").value,
-        endTag = document.getElementById("endtag").value;
+        endTag = document.getElementById("endtag").value,
+        wholeText = document.getElementById("tags").checked;
 
-    for(let i = 0; i < str.length; i++)
-    {
-        if(str[i] == startTag && start == 0) {
-            out += str.substr(newBegin, i - newBegin);
-            if(coloured && !rainbow)
-                out += `[color=${col}]`;
-            if(bold)
-                out += `[b]`;
-            if(italic)
-                out += `[i]`;
-            if(underline)
-                out += `[u]`;
-            if(strikethrough)
-                out += `[s]`;
-            if(sub)
-                out += `[sub]`;
-            if(sup)
-                out += `[sup]`;
+    col = document.getElementById("colour").value;
+    bold = document.getElementById("bold").checked;
+    italic = document.getElementById("italic").checked;
+    underline = document.getElementById("underline").checked;
+    strikethrough = document.getElementById("strikethrough").checked;
+    coloured = !isBlank(col);
+    rainbow = document.getElementById("rainbow").checked;
+    sub = document.getElementById("sub").checked;
+    sup = document.getElementById("super").checked;
 
-            newBegin = i;
-            start = 1;
+    if(wholeText){
+        out += startTags();
+        if(rainbow)
+            out += rainbowText(str);
+        else
+            out += str;
+        out += endTags();
+    } else {
+        for(let i = 0; i < str.length; i++) {
+            if(str[i] == startTag && start == 0) {
+                out += str.substr(newBegin, i - newBegin);
+                out += startTags();
+
+                newBegin = i;
+                start = 1;
+            } else if(str[i] == endTag && start != 0) {
+                if(rainbow)
+                    out += rainbowText(str.substr(newBegin, i - newBegin + 1));
+                else
+                    out += str.substr(newBegin, i - newBegin + 1);
+
+                out += endTags();
+
+                newBegin = i + 1;
+                start = 0;  
+            }
         }
-        else if(str[i] == endTag && start != 0) {
-            if(rainbow)
-                out += rainbowText(str.substr(newBegin, i - newBegin + 1));
-            else
-                out += str.substr(newBegin, i - newBegin + 1);
 
-            if(sup)
-                out += `[/sup]`;
-            if(sub)
-                out += `[/sub]`;
-            if(strikethrough)
-                out += `[/s]`;
-            if(underline)
-                out += `[/u]`;
-            if(italic)
-                out += `[/i]`;
-            if(bold)
-                out += `[/b]`;
-            if(coloured && !rainbow)
-                out += `[/color]`;
+        if(rainbow && start != 0)
+            out += rainbowText(str.substr(newBegin));
+        else
+            out += str.substr(newBegin);
 
-            newBegin = i + 1;
-            start = 0;  
+        //if a closing " is missing, close it at the end.
+        if(start != 0){
+            out += endTags();
         }
-        
-    }
-    if(rainbow && start != 0)
-        out += rainbowText(str.substr(newBegin));
-    else
-        out += str.substr(newBegin);
-
-    //if a closing " is missing, close it at the end.
-    if(start != 0){
-        if(strikethrough)
-            out += `[/s]`;
-        if(underline)
-            out += `[/u]`;
-        if(italic)
-            out += `[/i]`;
-        if(bold)
-            out += `[/b]`;
-        if(coloured && !rainbow)
-            out += `[/color]`;
     }
 
     let tmpout = replaceLinebreaks(out)
