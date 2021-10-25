3f63c43bcc1b812d4797be88dfd51f68d5802906
Jan Hoffmann <hoffmann@student.tugraz.at>
2021-10-25 03:04:22 +0200

    Rainbow now ignores space!

--- FList/flist_util.js
+++ FList/flist_util.js
@@ -1,8 +1,13 @@
 function rainbowText(str) {
     const col = ["red", "orange", "yellow", "green", "cyan", "blue", "purple"];
     let res = "";
-    for (let i = 0; i < str.length; i++)
+    for (let i = 0; i < str.length; i++){
+        if(str[i] == " ") {
+            res += str[i];
+            continue;
+        }
         res += `[color=${col[i % col.length]}]${str[i]}[/color]`;
+    }
 
     return res;
 }
