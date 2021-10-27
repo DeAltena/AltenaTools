8584c82f9bea9f86d1a2732b6bd77892a720357f
Charles de Altena <crowleyastray@gmail.com>
2021-10-27 12:00:01 +0200

    added extended rainbow function

--- FList/flist_util.js
+++ FList/flist_util.js
@@ -1,5 +1,14 @@
 function rainbowText(str) {
     const col = ["red", "orange", "yellow", "green", "cyan", "blue", "purple"];
+    return alternateColourText(col, str);
+}
+
+function gothbowText(str) {
+    const col = ["gray", "white", "black"];
+    return alternateColourText(col, str);
+}
+
+function alternateColourText(col, str) {
     let res = "";
     for (let i = 0; i < str.length; i++){
         if(str[i] == " ") {
